d3ada11ff25fc319b96ec0f2d0c9cc47dbb9dd4b
Pim van Helvoirt <pim.van.helvoirt@home.nl>
2023-06-10 15:14:57 +0200

    feat: add docs, added additional test, cleaned up test comments

--- molecule/default/tests/test_result.py
+++ molecule/default/tests/test_result.py
@@ -8,13 +8,20 @@ def test_os_release(host):
 
 
 def test_wireguard_present(host):
-    """test that the ssh service is running"""
+    """test that wireguard is present as command"""
 
     assert host.run("which wg").rc == 0
 
 
 def test_wg_config_file(host):
-    """test that the ssh service is running"""
+    """test that the wireguard config file is present"""
 
     with host.sudo():
         assert host.file("/etc/wireguard/wg0.conf")
+
+
+def test_peer_present(host):
+    """test that the peer is present in the wireguard config file"""
+
+    with host.sudo():
+        assert host.file("/etc/wireguard/wg0.conf").contains("test-peer")
